2b5642b5af5b5690a02dbfc38f6d737bdb47f5f5
Narek Hovhannisyan <narek.hovhannisyan.swe@gmail.com>
2024-10-23 08:32:40 +0400

    Add StarRating and TextExander components.

---
+++ src/components/TextExpander.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export default function TextExpander({
+  collapsedNumWords = 20,
+  expandButtonText = 'Show text',
+  collapseButtonText = 'Collapse text',
+  buttonColor = 'ff6622',
+  children,
+}) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleExpandClick = () => setCollapsed(collapsed => !collapsed);
+
+  const text = collapsed ? (children || '').split(' ').slice(0, collapsedNumWords).join(' ') + '...' : children;
+
+  return (
+    <div>
+      {text}{' '}
+      <span style={{ color: buttonColor, cursor: 'pointer' }} onClick={handleExpandClick}>
+        {collapsed ? expandButtonText : collapseButtonText}
+      </span>
+    </div>
+  );
+}
