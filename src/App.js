532f960b878bb25613bedab6c305c11504b08958
Narek Hovhannisyan <narek.hovhannisyan.swe@gmail.com>
2024-11-10 15:39:01 +0400

    Read query from search bar.

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
-import { useEffect, useState } from 'react';
-import StarRating from './components/StarRating';
-import TextExpander from './components/TextExpander';
-
-const tempMovieData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt0133093',
-    Title: 'The Matrix',
-    Year: '1999',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt6751668',
-    Title: 'Parasite',
-    Year: '2019',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-  },
-];
+import { useEffect, useState } from "react";
+import StarRating from "./components/StarRating";
+import TextExpander from "./components/TextExpander";
 
 const tempWatchedData = [
   {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
     runtime: 148,
     imdbRating: 8.8,
     userRating: 10,
   },
   {
-    imdbID: 'tt0088763',
-    Title: 'Back to the Future',
-    Year: '1985',
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
     Poster:
-      'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
     runtime: 116,
     imdbRating: 8.5,
     userRating: 9,
@@ -61,9 +37,7 @@ function Logo() {
   );
 }
 
-function Search() {
-  const [query, setQuery] = useState('');
-
+function Search({ query, setQuery }) {
   return (
     <input
       className="search"
@@ -83,11 +57,11 @@ function NavbarResult({ moviesCount }) {
   );
 }
 
-function Navbar({ moviesCount }) {
+function Navbar({ moviesCount, query, setQuery }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
+      <Search query={query} setQuery={setQuery} />
       <NavbarResult moviesCount={moviesCount} />
     </nav>
   );
@@ -187,7 +161,7 @@ function Box({ children }) {
   return (
     <div className="box">
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
-        {isOpen ? '–' : '+'}
+        {isOpen ? "–" : "+"}
       </button>
       {isOpen && children}
     </div>
@@ -210,29 +184,30 @@ function ErrorMessage({ message }) {
   );
 }
 
-const OMDB_KEY = '2b39c271';
-
+const OMDB_KEY = "2b39c271";
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState(tempWatchedData);
   const [isLoading, setIsLoading] = useState(false);
-  const [fetchingError, setFetchingError] = useState('');
+  const [fetchingError, setFetchingError] = useState("");
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchMoves = async () => {
       try {
+        setFetchingError("");
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_KEY}&s=pig`,
+          `http://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_KEY}&s=${query}`
         );
 
         if (!res.ok) {
-          throw new Error('Something went wrong with fetching movies');
+          throw new Error("Something went wrong with fetching movies");
         }
 
         const data = await res.json();
-        if (data.Response === 'False') {
-          throw new Error('Movie not found');
+        if (data.Response === "False") {
+          throw new Error("Movie not found");
         }
 
         setMovies(data.Search);
@@ -243,12 +218,19 @@ export default function App() {
         setIsLoading(false);
       }
     };
+
+    if (query.length < 3) {
+      setMovies([]);
+      setFetchingError("");
+      return;
+    }
+
     fetchMoves();
-  }, []);
+  }, [query]);
 
   return (
     <>
-      {/* <Navbar moviesCount={movies.length} /> */}
+      <Navbar moviesCount={movies.length} query={query} setQuery={setQuery} />
       <Main>
         <Box>
           {isLoading && <Loader />}
